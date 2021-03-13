16789479dd96ee1270e40edf877c9207095e5711
anderRSTN59 <58879565+anderRSTN59@users.noreply.github.com>
2021-03-13 02:15:38 +0100

    Correccion de errores y optimización

--- MyGraph.py
+++ MyGraph.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from xml.dom import minidom
-from os import listdir
+#import networkx as nx
+#import matplotlib.pyplot as plt
 from MyNode import MyNode
 from MyEdge import MyEdge
 
 class MyGraph:
+    '''implementacion de clase grafo'''
     name = ""
     node_list = ""
     edge_list = ""
@@ -17,7 +19,7 @@ class MyGraph:
         self.edge_list = edge_list
 
     def graph_data_line(self):
-        '''<Población>,<nº nodos>,<nº arcos>,<Longitud total de los arcos>,<nº coord X pares>,<nº de coord Y impares>'''
+        '''población, nº nodos, nº arcos, longitud total, nº X pares, nº Y impares'''
         population = self.name.split(",")
         n_nodes = len(self.node_list)
         n_edges = len(self.edge_list)
@@ -25,7 +27,7 @@ class MyGraph:
         even_x_cosrds = 0
         odd_y_cosrds = 0
         for edge in self.edge_list:
-            edge_total_len = edge_total_len + float(edge.data["length"])
+            edge_total_len = edge_total_len + float(edge.length)
         for node in self.node_list:
             node_coords = node.pass_to_integer_coords()
             if (node_coords[0] % 2) == 0:
@@ -37,58 +39,69 @@ class MyGraph:
 def read_xmlfile (xmlfile):
     '''lee el archivo .graphml'''
     xmldom = minidom.parse(open(xmlfile))
-    graph_nodes_data = xmldom.getElementsByTagName("node")
-    graph_edges_data = xmldom.getElementsByTagName("edge")
-    graph_keys_data = xmldom.getElementsByTagName("key")
-    graph_keys = read_keys (graph_keys_data)
-    node_list = read_nodes(xmlfile, graph_keys, graph_nodes_data)
-    edge_list = read_edges(xmlfile, graph_keys, graph_edges_data)
-
-    return MyGraph (get_graph_name, node_list, edge_list)
-
-def read_keys (graph_keys_data):
-    '''lee las keys del grafo contenidas en el .graphml'''
-    keys = []
-    for key in graph_keys_data:
-        keys.append([key.getAttribute("id"),key.getAttribute("attr.name")])
-    return keys
+    node_list = read_nodes(xmldom)
+    edge_list = read_edges(xmldom)
+    graph_name = get_graph_name(xmldom)
+    return MyGraph (graph_name, node_list, edge_list)
 
 def get_graph_name (xmldom): #revisar
     '''genera el grafo del .graphml'''
     name = ""
     graph_data = xmldom.getElementsByTagName("data")
-    graph_data = graph_data[len(graph_data)-4:]
-    for data in graph_data:
-        if data.getAttribute("key") == "d0":
-            name = data.firstChild.data
-    
-    return name
+    data = graph_data[len(graph_data)-4]
+    name = data.firstChild.data
+    return name.split(",")[0]
 
-def read_nodes (xmldom, graph_keys, graph_nodes_data):
+def read_nodes (xmldom):
     '''lee los nodos del grafo y genera una lista de nodos'''
+    graph_nodes = xmldom.getElementsByTagName("node")
     nodes = []
-    for node in graph_nodes_data:
+    for node in graph_nodes:
         new_node = MyNode(node.getAttribute("id"))
-        nodes.append(read_object_attr (graph_keys, new_node, node.getElementsByTagName("data")))
+        node_data = node.getElementsByTagName("data")
+        coord_x = float(node_data[1].firstChild.data)
+        coord_y = float(node_data[0].firstChild.data)
+        new_node.node_coords = coord_x, coord_y
+        nodes.append(new_node)
     return nodes
 
-def read_edges (xmldom, graph_keys, graph_edges_data):
+def read_edges (xmldom):
     '''lee las aristas del grafo y genera una lista de aristas'''
+    graph_edges = xmldom.getElementsByTagName("edge")
+    length_id, geometry_id = read_edge_keys (xmldom)
     edges = []
-    for edge in graph_edges_data:
+    for edge in graph_edges:
         new_edge = MyEdge(
             edge.getAttribute("id"),
             edge.getAttribute("source"),
             edge.getAttribute("target"))
-        edges.append(read_object_attr (graph_keys, new_edge, edge.getElementsByTagName("data")))
+        edge_attr_list = edge.getElementsByTagName("data")
+        for attr in edge_attr_list:
+            if length_id == attr.getAttribute("key"):
+                new_edge.length = float(attr.firstChild.data)
+            elif geometry_id == attr.getAttribute("key"):
+                new_edge.geometry = get_edge_geometry(attr.firstChild.data)
+        edges.append(new_edge)
     return edges
 
-def read_object_attr (graph_keys, element, attribute_list):
-    '''lee los atributos/datos de los objetos del grafo'''
-    data = {}
-    for attr in attribute_list:
-        for key in graph_keys:
-            if key[0] == attr.getAttribute("key"):
-                data[key[1]] = attr.firstChild.data
-    element.data = data
-    return element
+def read_edge_keys (xmldom):
+    '''lee las keys del grafo contenidas en el .graphml'''
+    graph_keys = xmldom.getElementsByTagName("key")
+    length_id = ""
+    geometry_id = ""
+    for key in graph_keys:
+        if key.getAttribute("attr.name") == "length":
+            length_id = key.getAttribute("id")
+        if key.getAttribute("attr.name") == "geometry":
+            geometry_id = key.getAttribute("id")
+    return length_id, geometry_id
+
+def get_edge_geometry (str_geometry):
+    '''leer la forma de la arista'''
+    section_list = str_geometry.split("(")[1].split(")")[0].split(", ")
+    geometry = []
+    for section in section_list:
+        aux = section.split(" ")
+        section_coords = float(aux[0]), float(aux[1])
+        geometry.append(section_coords)
+    return geometry
