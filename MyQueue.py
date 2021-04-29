02cd5720240778b75d417b5b30d5718b40e6be99
anderRSTN59 <58879565+anderRSTN59@users.noreply.github.com>
2021-04-29 17:20:47 +0200

    operaciones elementales de la ordenacion

--- MyQueue.py
+++ MyQueue.py
@@ -9,41 +9,42 @@ class MyQueue:
     def __init__(self):
         self.element_list = []
 
-    def copy_list (self, origin_list):
+    def copy_list (self, origin_list):                                      #2
         '''copia una lista de elementos a otra y luego la ordena'''
-        for new_element in origin_list:
-            self.element_list.append(new_element)
-        self.first_quick_sort_call()
+        for new_element in origin_list:                                     #bucles = n
+        # for (i = 0; i<len(origin_list); i++)                               6
+            self.element_list.append(new_element)                           #3
+        self.first_quick_sort_call()                                        #1
 
-    def add_element (self, element):
+    def add_element (self, element):                                        #2
         '''añade un nuevo elemento a la lista y lo ordena'''
-        self.element_list.append(element)
-        self.first_quick_sort_call()
+        self.element_list.append(element)                                   #3
+        self.first_quick_sort_call()                                        #T(n)
 
-    def first_quick_sort_call (self):
+    def first_quick_sort_call (self):                                       #1
         '''primera llamada del quicksort'''
-        self.my_quick_sort (0, len(self.element_list)-1)
+        self.my_quick_sort (0, len(self.element_list)-1)                    #T(n) + 4
 
-    def my_quick_sort (self, left, rigth):
+    def my_quick_sort (self, left, rigth):                                  #3
         '''implementacion del metodo quicksort'''
-        pivote = self.element_list[left]
-        i = left
-        j = rigth
+        pivote = self.element_list[left]                                    #3
+        i = left                                                            #2
+        j = rigth                                                           #2
 
-        while i < j:
-            while (self.element_list[i].length <= pivote.length) & (i < j):
-                i = i + 1
-            while self.element_list[j].length > pivote.length:
-                j = j - 1
-            if i < j:
-                aux = self.element_list[i]
-                self.element_list[i] = self.element_list[j]
-                self.element_list[j]= aux
+        while i < j:                                                        #2
+            while (self.element_list[i].length <= pivote.length) & (i < j): #7
+                i = i + 1                                                   #2
+            while self.element_list[j].length > pivote.length:              #5
+                j = j - 1                                                   #2
+            if i < j:                                                       #1
+                aux = self.element_list[i]                                  #3
+                self.element_list[i] = self.element_list[j]                 #5
+                self.element_list[j] = aux                                  #3
 
-        self.element_list[left] =  self.element_list[j]
-        self.element_list[j] = pivote
+        self.element_list[left] = self.element_list[j]                      #5
+        self.element_list[j] = pivote                                       #3
 
-        if left < (j-1):
-            self.my_quick_sort (left, j-1)
-        if (j+1) < rigth:
-            self.my_quick_sort (j+1, rigth)
+        if left < (j-1):                                                    #2
+            self.my_quick_sort (left, j-1)                                  #T(n/2) + 3
+        if (j+1) < rigth:                                                   #2
+            self.my_quick_sort (j+1, rigth)                                 #T(n/2) + 3
